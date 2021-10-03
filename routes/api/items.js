ebf6165adab74392d8e59d40bb9bcf366c01c097
Ge0h <george.walker.wme@gmail.com>
2021-10-03 12:29:15 +0100

    Item database complete

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,30 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 })
 
+// @route   POST api/item
+// @descr   Create an item 
+// @access  Public 
+router.post('/', (req, res) => {
+    const newItem = new Items({
+        equipment: req.body.equipment,
+        description: req.body.description,
+        supplier: req.body.supplier,
+        category: req.body.category,
+        quantity: req.body.quantity,
+        url: req.body.url,
+        price: req.body.price
+    });
+
+    newItem.save().then(item => res.json(item));
+})
+
+// @route   DELETE api/item
+// @descr   Delete an item 
+// @access  Public
+router.delete('/:id', (req, res) => {
+    Items.findById(req.params.id)
+        .then(item => item.remove().then(() => res.json({ success: true })))
+        .catch(err => res.status(404).json({ success: false }))
+    })
+
 module.exports = router;
