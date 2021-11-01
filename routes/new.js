40526c3a92e203f3552167c85ddc80d6bd09f939
Ge0h <george.walker.wme@gmail.com>
2021-11-01 16:39:18 +0000

    Changed snew form supplier / category to drop down

--- routes/new.js
+++ routes/new.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const Suppliers = require('../models/Supplier');
+const Category = require('../models/Category');
 
 /* GET new page. */
 router.get('/', (req, res, next) => {
-  res.render('new', { 
-    title: "New Item" 
-  });
+  Suppliers.find()
+    .exec((err, suppliers) => {
+      Category.find().exec((err, categorys) => {
+        if (err) console.log(err)
+        res.render('new', { 
+          title: "New Item",
+          suppliers,
+          categorys
+      });
+    })
+  })
 });
 
 module.exports = router;
